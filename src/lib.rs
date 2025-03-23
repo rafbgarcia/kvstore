7b7f294679284701f2530554b2ad78d19e946764
Rafael Garcia <rafbgarcia@gmail.com>
2025-03-23 15:38:37 -0300

    Add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
+#![deny(missing_docs)]
+#![doc = "module"]
+
+/// KvStore is a simple in memory key-value store.
 pub struct KvStore {
     store: std::collections::HashMap<String, String>,
 }
 
 impl KvStore {
+    /// ```rust
+    /// use kvs::KvStore;
+    /// let mut store = KvStore::new();
+    /// ```
     pub fn new() -> Self {
         KvStore {
             store: std::collections::HashMap::new(),
         }
     }
 
+    /// Get the value of a key
+    /// ```rust
+    /// use kvs::KvStore;
+    /// let mut store = KvStore::new();
+    /// store.set("key1".to_owned(), "value1".to_owned());
+    /// store.get("key1".to_owned());
+    /// ```
     pub fn get(self: &Self, key: String) -> Option<String> {
         if self.store.contains_key(key.as_str()) {
             return Some(self.store.get(key.as_str()).unwrap().to_string());
@@ -17,10 +32,23 @@ impl KvStore {
         return None;
     }
 
+    /// Get the value of a key
+    /// ```rust
+    /// use kvs::KvStore;
+    /// let mut store = KvStore::new();
+    /// store.set("key1".to_owned(), "value1".to_owned());
+    /// ```
     pub fn set(self: &mut Self, key: String, value: String) {
         self.store.insert(key, value);
     }
 
+    /// Get the value of a key
+    /// ```rust
+    /// use kvs::KvStore;
+    /// let mut store = KvStore::new();
+    /// store.set("key1".to_owned(), "value1".to_owned());
+    /// store.remove("key1".to_owned());
+    /// ```
     pub fn remove(self: &mut Self, key: String) {
         self.store.remove(key.as_str());
     }
